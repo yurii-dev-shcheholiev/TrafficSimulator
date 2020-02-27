3e717f21737e41df36d57056be27f6285877ccd4
Andrés Salazar Colina <Andres@eduroam126224.eduroam.ucm.es>
2020-02-27 14:13:37 +0100

    Minor Mistake Fixed

--- src/simulator/model/RoundRobinStrategy.java
+++ src/simulator/model/RoundRobinStrategy.java
@@ -3,23 +3,21 @@ package simulator.model;
 import java.util.List;
 
 public class RoundRobinStrategy implements LightSwitchingStrategy {
-	
+
 	private int _timeSlot;
-	
-	public RoundRobinStrategy(int timeSlot) {
-		
-		_timeSlot = timeSlot;
-	}
+
+	public RoundRobinStrategy(int timeSlot){ _timeSlot = timeSlot; }
 
 	@Override
-	public int chooseNextGreen(List<Road> roads, List<Vehicle> qs, int currGreen, int lastSwitchingTime, int currTime) {
+	public int chooseNextGreen(List<Road> roads, List<List<Vehicle>> qs, int currGreen, int lastSwitchingTime, int currTime) {
 		
 		if (roads.isEmpty()) return -1;
 		
 		if(currGreen == -1) return 0;
 		
 		if ((currTime - lastSwitchingTime) < _timeSlot) return currGreen;
-		else return ((currGreen + 1) % roads.size()); //TODO: It should be revised. I believe that there's more to add. To consult with teacher
+
+		return ((currGreen + 1) % roads.size());
 	}
 
 }
