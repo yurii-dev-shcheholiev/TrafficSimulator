83ae5e3991f26f735c6830fddecfa94b8bf7efa0
yurii-dev-shcheholiev <yurii.shcheholiev@gmail.com>
2020-03-04 10:52:31 +0100

    MostCrowdedSBuilder

---
+++ src/simulator/factories/MostCrowdedStrategyBuilder.java
@@ -0,0 +1,24 @@
+package simulator.factories;
+
+import org.json.JSONObject;
+import simulator.model.LightSwitchingStrategy;
+import simulator.model.MostCrowdedStrategy;
+import simulator.model.RoundRobinStrategy;
+
+public class MostCrowdedStrategyBuilder extends Builder<LightSwitchingStrategy>{
+    MostCrowdedStrategyBuilder(String type) {
+        super(type);
+    }
+
+    @Override
+    protected LightSwitchingStrategy createTheInstance(JSONObject data) {
+        int i = 0;
+        try {
+            i = (data.getInt("timeslot"));
+        } catch(NullPointerException | ClassCastException e) {
+            System.out.println("The JSON object is incorrect" + e.getMessage());
+            return null;
+        }
+        return new MostCrowdedStrategy(i);
+    }
+}
