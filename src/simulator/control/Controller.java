9a73f178aa9bd1b7f5e4ee156558b2b54006aea8
yurii-dev-shcheholiev <yurii.shcheholiev@gmail.com>
2020-03-04 22:10:29 +0100

    Controller unfinished

--- src/simulator/control/Controller.java
+++ src/simulator/control/Controller.java
@@ -1,4 +1,32 @@
 package simulator.control;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
+import simulator.factories.Factory;
+import simulator.model.Event;
+import simulator.model.TrafficSimulator;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class Controller {
+    private TrafficSimulator _trafSim;
+    private Factory<Event> _evFactory;
+
+    public Controller(TrafficSimulator sim, Factory<Event> eventsFactory) {
+        _trafSim = sim;
+        _evFactory = eventsFactory;
+    }
+
+    public void loadEvents(InputStream in) {
+        JSONObject jo = new JSONObject(new JSONTokener(in));
+    }
+
+    public void run(int n, OutputStream out) {
+
+    }
+
+    public void reset() {
+        _trafSim = new TrafficSimulator();
+    }
 }
