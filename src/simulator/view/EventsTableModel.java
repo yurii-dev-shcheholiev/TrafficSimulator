6129c03cebdfff6bf103bef43e987e88874374f3
Andrés Salazar Colina <Andres@MacBook-Pro-de-Andres-3.local>
2020-05-06 00:42:03 +0200

    Changes to some classes as well as implementation of TableModels + Status Bar

---
+++ src/simulator/view/EventsTableModel.java
@@ -0,0 +1,68 @@
+package simulator.view;
+
+import simulator.control.Controller;
+import simulator.model.Event;
+import simulator.model.RoadMap;
+import simulator.model.TrafficSimObserver;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+
+public class EventsTableModel extends AbstractTableModel implements TrafficSimObserver {
+
+    private Controller _ctrl;
+    private Object[][] eTable;
+    public EventsTableModel(Controller ctrl){
+        super();
+        _ctrl = ctrl;
+        eTable = new Object[50][50];
+        _ctrl.addObserver(this);
+    }
+
+    @Override
+    public int getRowCount() {
+        return eTable[0].length;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return eTable.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return eTable[rowIndex][columnIndex];
+    }
+
+    @Override
+    public void onAdvanceStart(RoadMap map, List<Event> events, int time) { table(events); }
+
+    @Override
+    public void onAdvanceEnd(RoadMap map, List<Event> events, int time) { table(events); }
+
+    @Override
+    public void onEventAdded(RoadMap map, List<Event> events, Event e, int time) { table(events); }
+
+    @Override
+    public void onReset(RoadMap map, List<Event> events, int time) { table(events); }
+
+    @Override
+    public void onRegister(RoadMap map, List<Event> events, int time) { table(events); }
+
+    @Override
+    public void onError(String err) {
+
+    }
+
+
+    private void table(List<Event> events){
+
+        for (int i = 0; i < events.size(); i++){
+            eTable[i][0] = events.get(i);
+            eTable[i][1] = events.get(i).getTime();
+            eTable[i][2] = events.get(i).toString();
+        }
+    }
+
+
+}
---
+++ src/simulator/view/RoadsTableModel.java
@@ -0,0 +1,81 @@
+package simulator.view;
+
+import simulator.control.Controller;
+import simulator.model.Event;
+import simulator.model.RoadMap;
+import simulator.model.TrafficSimObserver;
+
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+
+public class RoadsTableModel extends AbstractTableModel implements TrafficSimObserver {
+
+    private Controller _ctrl;
+    private Object[][] rTable;
+    public RoadsTableModel(Controller ctrl){
+        super();
+        _ctrl = ctrl;
+        rTable = new Object[50][50];
+        _ctrl.addObserver(this);
+    }
+
+    @Override
+    public int getRowCount() {
+        return rTable[0].length;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return rTable.length;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return rTable[rowIndex][columnIndex];
+    }
+
+    @Override
+    public void onAdvanceStart(RoadMap map, List<Event> events, int time) {
+        table(map);
+    }
+
+    @Override
+    public void onAdvanceEnd(RoadMap map, List<Event> events, int time) {
+        table(map);
+    }
+
+    @Override
+    public void onEventAdded(RoadMap map, List<Event> events, Event e, int time) {
+
+    }
+
+    @Override
+    public void onReset(RoadMap map, List<Event> events, int time) { table(map); }
+
+    @Override
+    public void onRegister(RoadMap map, List<Event> events, int time) {
+
+    }
+
+    @Override
+    public void onError(String err) {
+
+    }
+
+
+    private void table(RoadMap map){
+
+        for (int i = 0; i < map.getRoads().size(); i++){
+
+                rTable[i][0] = map.getRoads().get(i);
+                rTable[i][1] = map.getRoads().get(i).getId();
+                rTable[i][2] = map.getRoads().get(i).getLength();
+                rTable[i][3] = map.getRoads().get(i).getPWeather();
+                rTable[i][4] = map.getRoads().get(i).getPMaxSpeed();
+                rTable[i][5] = map.getRoads().get(i).getSpeedLimit();
+                rTable[i][6] = map.getRoads().get(i).getTotalContamination();
+                rTable[i][7] = map.getRoads().get(i).getPContLimit();
+        }
+    }
+
+}
