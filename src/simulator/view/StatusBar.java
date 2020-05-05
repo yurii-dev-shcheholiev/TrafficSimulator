ca43720de63971f0067aa5645806783b421e4dd6
Andrés Salazar Colina <Andres@MacBook-Pro-de-Andres-3.local>
2020-05-05 17:31:50 +0200

    Changes to the StatusBar

--- src/simulator/view/StatusBar.java
+++ src/simulator/view/StatusBar.java
@@ -6,45 +6,70 @@ import simulator.model.RoadMap;
 import simulator.model.TrafficSimObserver;
 
 import javax.swing.*;
+import java.awt.*;
 import java.util.List;
 
 public class StatusBar extends JPanel implements TrafficSimObserver {
 
     private Controller _ctrl;
+    private int _currTime;
+    private String _event;
+    private JLabel _timeLabel;
+    //private JPanel _Status;
+    private JLabel _eventLabel;
 
     StatusBar(Controller ctrl) {
         super();
         _ctrl = ctrl;
+        _currTime = 0;
+        _event = null;
 
         initGUI();
+
+        _ctrl.addObserver(this);
     }
 
     private void initGUI() {
 
+        _timeLabel = new JLabel(String.valueOf(_currTime));
+        _timeLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
+
+        _eventLabel = new JLabel("Event Added " + _event);
+        _eventLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        this.add(_timeLabel, _eventLabel);
 
     }
     @Override
     public void onAdvanceStart(RoadMap map, List<Event> events, int time) {
 
+        _currTime = time;
     }
 
     @Override
     public void onAdvanceEnd(RoadMap map, List<Event> events, int time) {
 
+        _currTime = time;
     }
 
     @Override
     public void onEventAdded(RoadMap map, List<Event> events, Event e, int time) {
+
+        _currTime = time;
+        _event = e.toString();
 
     }
 
     @Override
     public void onReset(RoadMap map, List<Event> events, int time) {
 
+        _currTime = time;
     }
 
     @Override
     public void onRegister(RoadMap map, List<Event> events, int time) {
+
+        _currTime = time;
 
     }
 
